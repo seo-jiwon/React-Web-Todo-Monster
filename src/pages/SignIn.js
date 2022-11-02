9aa3422b23c82ea1bb5f17c28a99820c5c888cdb
myg <72336796+mayungyeong@users.noreply.github.com>
2022-11-02 15:58:44 +0900

    SignUp, SignIn 페이지 View 작성

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -1,12 +1,42 @@
-import React from "react";
+import { React } from "react";
 import { useNavigate } from "react-router-dom";
+import "../css/SignIn.css";
 
 function SignIn() {
   const navigate = useNavigate();
 
   return (
-    <div>
-      <p>SignIn</p>
+    <div id="container">
+      <div id="AppBar">
+        <button
+          id="backBtn"
+          onClick={() => {
+            navigate("/Home");
+          }}
+        >
+          {"<"}
+        </button>
+        <div id="pageTitle">로그인</div>
+      </div>
+
+      <div id="inputForm">
+        <input id="value" placeholder="이메일"></input>
+        <div id="line"></div>
+      </div>
+      <div id="inputForm">
+
+        <input id="value" placeholder="비밀번호"></input>
+        <div id="line"></div>
+      </div>
+
+      <button
+        id="checkBtn"
+        onClick={() => {
+          navigate("/");
+        }}
+      >
+        확인
+      </button>
     </div>
   );
 }
--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,12 +1,42 @@
-import React from "react";
+import { React } from "react";
 import { useNavigate } from "react-router-dom";
+import "../css/SignIn.css";
 
 function SignUp() {
   const navigate = useNavigate();
 
   return (
-    <div>
-      <p>SignUp</p>
+    <div id="container">
+      <div id="AppBar">
+        <button
+          id="backBtn"
+          onClick={() => {
+            navigate("/Home");
+          }}
+        >
+          {"<"}
+        </button>
+        <div id="pageTitle">회원가입</div>
+      </div>
+
+      <div id="inputForm">
+        <input id="value" placeholder="이메일"></input>
+        <div id="line"></div>
+      </div>
+      <div id="inputForm">
+
+        <input id="value" placeholder="비밀번호"></input>
+        <div id="line"></div>
+      </div>
+
+      <button
+        id="checkBtn"
+        onClick={() => {
+          navigate("/Home");
+        }}
+      >
+        확인
+      </button>
     </div>
   );
 }
