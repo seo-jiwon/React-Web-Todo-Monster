b81b7bae124263241c30af1c43e93816e340b11d
kkoltong01 <kkoltong015@gmail.com>
2022-11-11 09:30:36 +0900

    cors 모듈 제거

--- server/server.js
+++ server/server.js
@@ -1,19 +1,21 @@
 const express = require('express');
 const app = express();
-const cors = require("cors");
 const bodyParser = require("body-parser");
+const cookie = require('cookie-parser')
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cookie())
 
 const user = require("./Router/user");
 const doInput = require("./Router/todolist");
 const search = require("./Router/search");
-
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-app.use(cors());
+const isLogged = require("./Router/isLogged");
 
 app.use("/user", user);
 app.use("/todolist", doInput);
 app.use("/search", search);
+app.use("/isLogged", isLogged);
 
 const port = 5000;
 app.listen(port, () => {
